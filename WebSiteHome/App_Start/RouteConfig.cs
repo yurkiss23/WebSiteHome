80db1e7b3f16f57bfb70984d6922d7fbc5ad4991
yurkiss <terranet23@gmail.com>
2019-08-22 02:05:35 +0300

    commit1

--- WebSiteHome/App_Start/RouteConfig.cs
+++ WebSiteHome/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebSiteHome
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Guitar", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
